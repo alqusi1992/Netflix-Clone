162aa615ec17ffd5c05c70610a0975659048bbc8
Mo Alqusi <malqusi@yahoo.com>
2021-10-30 16:56:18 +0200

    finished the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NavStyle.css';
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
   return (
-    <div className='nav'>
+    <div className={`nav ${show && 'nav-black'}`}>
       <img
         className='nav-logo'
         src='https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=35'
